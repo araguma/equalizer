7c2afc12ba748babcb8b186a9377d1993a894ac5
araguma <itetbitb@gmail.com>
2023-07-11 12:33:24 -0400

    feat: Add more equalizer presets

--- src/scripts/popup.ts
+++ src/scripts/popup.ts
@@ -5,7 +5,13 @@ const presetGains: {
     [preset: string]: number[];
 } = {
     'none': [],
-    'bass-boost': [6, 5, 4, 3, 2, 1, 0, 0, 0, 0],
+    'acoustic': [4, 4, 3, 1, 2, 2, 3, 4, 3, 2],
+    'electronic': [4, 3, 1, 0, -2, 3, 1, 2, 4, 5],
+    'latin': [4, 3, 0, 0, -1, -1, -1, 0, 3, 4],
+    'piano': [3, 2, 0, 3, 3, 1, 3, 4, 3, 3],
+    'pop': [-2, -1, 0, 2, 4, 4, 2, 0, -1, -2],
+    'rock': [5, 4, 3, 1, 0, -1, 1, 3, 4, 5],
+    'bass-boost': [5, 4, 3, 2, 1, 0, 0, 0, 0, 0],
 }
 
 const websiteDisplay = document.getElementById('website') as HTMLDivElement;
